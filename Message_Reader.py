3249ec49cd03205b9442e97a9e680fe0b5ff27eb
Steven Effland <seffland@gmail.com>
2014-09-13 09:37:01 -0400

    Added a running process to monitor the dir

--- Message_Reader.py
+++ Message_Reader.py
@@ -1,12 +1,23 @@
 __author__ = 'Steven Effland'
 import os.path
+import time
 
-startingNum = raw_input("Input starting alert number")
+# startingNum = raw_input("Input starting alert number")
+#
+# filepath = "C:/Users/seffl_000/Documents/WORK/Message-Reader/"
+# endText = "_alert.txt"
+#
+# #Checks to see if file exists
+# print "Searching for: " + startingNum + endText
+# print os.path.isfile(filepath+startingNum+endText)
 
-filepath = "C:/Users/seffl_000/Documents/WORK/"
-endText = "_alert.txt"
-
-#Checks to see if file exists
-print "Searching for: " + startingNum + endText
-print os.path.isfile(filepath+startingNum+endText)
-
+path_to_watch = "C:/Users/seffl_000/Documents/WORK/Message-Reader/"
+before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+while 1:
+  time.sleep (10)
+  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+  added = [f for f in after if not f in before]
+  removed = [f for f in before if not f in after]
+  if added: print "Added: ", ", ".join (added)
+  if removed: print "Removed: ", ", ".join (removed)
+  before = after
