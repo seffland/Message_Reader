beaa2a734ede06be4ea15ad3c9cc9bc0bb5b2870
Steven Effland <seffland@gmail.com>
2014-09-16 11:50:58 -0400

    added a function that reads all the files in the dir. going to add a feature to check if it is the newest file.

--- Message_Reader.py
+++ Message_Reader.py
@@ -6,16 +6,23 @@ import time
 def start(argument):
     print "Starting fortran program with ", ": ".join(argument)
 
-path_to_watch = "C:/Users/seffl_000/Documents/WORK/Message-Reader/"
+
+def newmsgcheck(path, dirpath):
+    filelist = []
+    filelist += [file for file in os.listdir(dirpath) if os.path.isfile(file)]
+    for item in filelist:
+        print item
+    return True
+
+
+path_to_watch = "C:\Users\seffl_000\Documents\Message_Reader"
 before = dict([(f, None) for f in os.listdir(path_to_watch)])
 while 1:
     time.sleep(10)
     after = dict([(f, None) for f in os.listdir(path_to_watch)])
     added = [f for f in after if not f in before]
-    removed = [f for f in before if not f in after]
     if added:
-        print "Added: ", ", ".join(added)
-        start(added)
-    if removed:
-        print "Removed: ", ", ".join(removed)
+        if newmsgcheck(added, path_to_watch):
+            print "Added: ", ", ".join(added)
+            start(added)
     before = after
