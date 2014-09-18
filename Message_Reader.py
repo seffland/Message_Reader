020a23c63422151a10d8f7b3b2137fc608b7dba4
Steven Effland <seffland@gmail.com>
2014-09-17 21:54:46 -0400

    Added new msg finder and 2 modes

--- Message_Reader.py
+++ Message_Reader.py
@@ -7,22 +7,46 @@ def start(argument):
     print "Starting fortran program with ", ": ".join(argument)
 
 
-def newmsgcheck(path, dirpath):
-    filelist = []
-    filelist += [file for file in os.listdir(dirpath) if os.path.isfile(file)]
-    for item in filelist:
-        print item
+def newalertcheck(list, new):
+    test = new[0]
+    if test > max(list):
+        print "New Alert!"
     return True
 
+def startoperational():
+    path_to_watch = "C:\Users\seffl_000\Documents\WORK\Message-Reader\Alerts"
+    filelist = []
+    #Creates list of files in path
+    for file in os.listdir(path_to_watch):
+        filelist.append(file[0:3])
 
-path_to_watch = "C:\Users\seffl_000\Documents\Message_Reader"
-before = dict([(f, None) for f in os.listdir(path_to_watch)])
-while 1:
-    time.sleep(10)
-    after = dict([(f, None) for f in os.listdir(path_to_watch)])
-    added = [f for f in after if not f in before]
-    if added:
-        if newmsgcheck(added, path_to_watch):
-            print "Added: ", ", ".join(added)
-            start(added)
-    before = after
+    before = dict([(f, None) for f in os.listdir(path_to_watch)])
+    while 1:
+        time.sleep(10)
+        after = dict([(f, None) for f in os.listdir(path_to_watch)])
+        newfile = [f for f in after if not f in before]
+        if newfile:
+            if newalertcheck(filelist, newfile):
+                print "new alert: ", ", ".join(newfile)
+                start(newfile)
+        before = after
+
+
+def startresearch():
+    start = raw_input("Starting file:")
+    end = raw_input("Ending file")
+
+
+mode = raw_input("Mode to start in: [O]perational or [R]esearch...")
+if mode == "O" or "o":
+    print("Operational mode running...")
+    startoperational()
+elif mode == "R" or "r":
+
+    startresearch()
+else:
+    print "Please input valid mode..."
+    mode = raw_input("Mode to start in: [O]perational or [R]esearch...")
+
+
+
