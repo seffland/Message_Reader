2d501b0c58fce09c59a32e4dd595f522e06d468e
Steven Effland <seffland@gmail.com>
2014-09-17 23:06:44 -0400

    Added config info

--- Message_Reader.py
+++ Message_Reader.py
@@ -1,7 +1,21 @@
 __author__ = 'Steven Effland'
 import os.path
 import time
+import ConfigParser
 
+#Built in config funct
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
 
 def start(argument):
     print "Starting fortran program with ", ": ".join(argument)
@@ -10,11 +24,12 @@ def start(argument):
 def newalertcheck(list, new):
     test = new[0]
     if test > max(list):
-        print "New Alert!"
-    return True
+        return True
+    else:
+        return False
 
 def startoperational():
-    path_to_watch = "C:\Users\seffl_000\Documents\WORK\Message-Reader\Alerts"
+    path_to_watch = ConfigSectionMap("File Options")['path']
     filelist = []
     #Creates list of files in path
     for file in os.listdir(path_to_watch):
@@ -33,16 +48,28 @@ def startoperational():
 
 
 def startresearch():
-    start = raw_input("Starting file:")
-    end = raw_input("Ending file")
+    maxprocnum = int(ConfigSectionMap("File Options")['maxfilesin'])
+    print ("Max number of alerts = "), maxprocnum
 
+    start = int(raw_input("Starting file: "))
+    end = int(raw_input("Ending file: "))
+    print ("end - start ="), (end - start)
+    if (end - start) > maxprocnum:
+        print("Too many files to proccess")
+    elif (end - start) < maxprocnum:
+        print("Starting fortran program from file:"), start, ("to"), end
+
+#Config file setup
+Config = ConfigParser.ConfigParser()
+configfile = "C:\Users\seffl_000\Documents\WORK\Message-Reader\config.ini"
+Config.read(configfile)
 
 mode = raw_input("Mode to start in: [O]perational or [R]esearch...")
-if mode == "O" or "o":
+if mode == "O" or mode == "o":
     print("Operational mode running...")
     startoperational()
-elif mode == "R" or "r":
-
+if mode == "R" or mode == "r":
+    print("Starting research mode...")
     startresearch()
 else:
     print "Please input valid mode..."
